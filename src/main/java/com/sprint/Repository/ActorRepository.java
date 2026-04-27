99d57d02666fcd6436201183bd89587312b60bc0
MohdAmaan07 <mohd.amaan8642@gmail.com>
2026-04-27 13:10:27 +0530

    fix: spring data rest to JSON from HAL

--- src/main/java/com/sprint/Repository/ActorRepository.java
+++ src/main/java/com/sprint/Repository/ActorRepository.java
@@ -2,8 +2,12 @@ package com.sprint.Repository;
 
 import com.sprint.Entities.Actor;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.rest.core.annotation.RepositoryRestResource;
+import java.util.List;
 
-@RepositoryRestResource(collectionResourceRel = "actors", path = "actors")
+@RepositoryRestResource(collectionResourceRel = "actors", path = "actors", exported = true)
 public interface ActorRepository extends JpaRepository<Actor, Long> {
+    @Query("SELECT a FROM Actor a WHERE LOWER(a.firstName) = LOWER(:firstName)")
+    List<Actor> findByFirstName(String firstName);
 }
